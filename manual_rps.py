a3fd2015365f0e64044c8dd54de549724d548b5c
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2023-01-08 23:26:08 +0000

    updated as per feedback to not accept parameters

--- manual_rps.py
+++ manual_rps.py
@@ -1,6 +1,7 @@
 import random
-rps = (["rock","paper","scissors"])
-def get_computer_choice(rps):
+
+def get_computer_choice():
+    rps = (["rock","paper","scissors"])
     computer_choice = random.choice(rps)
     return computer_choice
 
@@ -8,4 +9,4 @@ def get_user_choice():
     user_choice = input (" Please choose between Rock, Paper or Scissors!. ")
     return user_choice
 
-get_user_choice()
+get_computer_choice()
