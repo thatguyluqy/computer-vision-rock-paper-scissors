2dd168c55b79182e84bcd375baf514e0ee88456e
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2023-01-09 00:30:50 +0000

    created get winner function

--- manual_rps.py
+++ manual_rps.py
@@ -9,4 +9,14 @@ def get_user_choice():
     user_choice = input (" Please choose between Rock, Paper or Scissors!. ")
     return user_choice
 
-get_computer_choice()
+def get_winner(computer_choice, user_choice):
+    if computer_choice == "Rock" and user_choice == "Scissors":
+        print("You lost")
+    elif computer_choice == "Paper" and user_choice == "Rock":
+        print("You lost")
+    elif computer_choice == "Scissors" and user_choice == "Paper":
+        print("You lost")
+    elif computer_choice == user_choice:
+        print("It is a tie!")
+    else:
+        print("You won!")
